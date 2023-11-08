a1c6767f839ecf4810a879fc11c3382cfaa37684
dung11122005 <145743207+dung11122005@users.noreply.github.com>
2023-11-08 18:08:19 +0700

    1

---
+++ animation/doimau.js
@@ -0,0 +1,8 @@
+const click=document.querySelector('.bam2')
+click.addEventListener("click",function(){
+   document.querySelector('.anh2').style.display="flex";
+})  
+const clicktat=document.querySelector('.bam1')
+clicktat.addEventListener("click",function(){
+   document.querySelector('.anh2').style.display="none";
+})  
